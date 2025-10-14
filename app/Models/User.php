598c80da3e2ce658cdf61ee6a237f810130b8ba3
Kevin Eduardo Morán Ramirez <dev@kmoransv.me>
2025-10-13 19:28:01 -0600

    Se crearon las migraciones, los seeders y los modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'tipos_id',
+        'username',
         'email',
         'password',
     ];
+
+    public $timestamps = false;
 
     /**
      * The attributes that should be hidden for serialization.
